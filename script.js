d585df3dfdff44758237524357ca65a7e10e4e92
Raphaell Monteiro <106750983+Raphaellmonteiro@users.noreply.github.com>
2025-11-01 16:04:13 -0300

    script.js

--- script.js
+++ script.js
@@ -48,4 +48,4 @@ document.addEventListener("DOMContentLoaded", function() {
     
     // ======================================= //
     console.log("Portfólio carregado com sucesso!");
-    });
+});
